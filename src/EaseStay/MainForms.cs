99364199c6e44150dfc3100aa0386a1d09a20bb1
Alcides Alípio <alcides.alipio.809@gmail.com>
2026-04-28 14:43:47 +0100

    feat: add project structure

---
+++ src/EaseStay/MainForms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EaseStay
+{
+    public partial class MainForms : Form
+    {
+        public MainForms()
+        {
+            InitializeComponent();
+        }
+    }
+}
