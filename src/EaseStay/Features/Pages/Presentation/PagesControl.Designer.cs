d35b94273fc63917f537fa1c6fe1b6698039f855
Alcides Alípio <alcides.alipio.809@gmail.com>
2026-05-07 16:19:15 +0100

    feat(PagesControl): add PagesControl component with layout and logout functionality

---
+++ src/EaseStay/Features/Pages/Presentation/PagesControl.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace EaseStay.Features.Pages.Presentation
+{
+    partial class PagesControl
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TblDashboardLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.TblMenuLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.TblMenuLogoLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.PBoxMenuLogo = new System.Windows.Forms.PictureBox();
+            this.LbMenuLogo = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.LbOptionsHeader1 = new System.Windows.Forms.Label();
+            this.LbOptionsHeader2 = new System.Windows.Forms.Label();
+            this.LbOptionsHeader3 = new System.Windows.Forms.Label();
+            this.LbOptionsHeader4 = new System.Windows.Forms.Label();
+            this.LbOptionsHeader5 = new System.Windows.Forms.Label();
+            this.LbOptionsHeader6 = new System.Windows.Forms.Label();
+            this.SplLogoOptions = new System.Windows.Forms.Splitter();
+            this.LbBtnLogout = new EaseStay.Core.Elements.LabelButtonStylized();
+            this.TblDashboardLayout.SuspendLayout();
+            this.TblMenuLayout.SuspendLayout();
+            this.TblMenuLogoLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxMenuLogo)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TblDashboardLayout
+            // 
+            this.TblDashboardLayout.ColumnCount = 2;
+            this.TblDashboardLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.TblDashboardLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.TblDashboardLayout.Controls.Add(this.TblMenuLayout, 0, 0);
+            this.TblDashboardLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TblDashboardLayout.Location = new System.Drawing.Point(0, 0);
+            this.TblDashboardLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.TblDashboardLayout.Name = "TblDashboardLayout";
+            this.TblDashboardLayout.RowCount = 1;
+            this.TblDashboardLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TblDashboardLayout.Size = new System.Drawing.Size(768, 512);
+            this.TblDashboardLayout.TabIndex = 1;
+            // 
+            // TblMenuLayout
+            // 
+            this.TblMenuLayout.ColumnCount = 3;
+            this.TblMenuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.TblMenuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.TblMenuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.TblMenuLayout.Controls.Add(this.TblMenuLogoLayout, 1, 0);
+            this.TblMenuLayout.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.TblMenuLayout.Controls.Add(this.SplLogoOptions, 1, 1);
+            this.TblMenuLayout.Controls.Add(this.LbBtnLogout, 1, 3);
+            this.TblMenuLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TblMenuLayout.Location = new System.Drawing.Point(0, 0);
+            this.TblMenuLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.TblMenuLayout.Name = "TblMenuLayout";
+            this.TblMenuLayout.RowCount = 4;
+            this.TblMenuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.TblMenuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.TblMenuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TblMenuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.TblMenuLayout.Size = new System.Drawing.Size(230, 512);
+            this.TblMenuLayout.TabIndex = 1;
+            // 
+            // TblMenuLogoLayout
+            // 
+            this.TblMenuLogoLayout.ColumnCount = 3;
+            this.TblMenuLogoLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.TblMenuLogoLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.TblMenuLogoLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TblMenuLogoLayout.Controls.Add(this.PBoxMenuLogo, 0, 1);
+            this.TblMenuLogoLayout.Controls.Add(this.LbMenuLogo, 1, 1);
+            this.TblMenuLogoLayout.Location = new System.Drawing.Point(23, 0);
+            this.TblMenuLogoLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.TblMenuLogoLayout.Name = "TblMenuLogoLayout";
+            this.TblMenuLogoLayout.RowCount = 3;
+            this.TblMenuLogoLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TblMenuLogoLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.TblMenuLogoLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.00001F));
+            this.TblMenuLogoLayout.Size = new System.Drawing.Size(184, 49);
+            this.TblMenuLogoLayout.TabIndex = 1;
+            // 
+            // PBoxMenuLogo
+            // 
+            this.PBoxMenuLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PBoxMenuLogo.Image = global::EaseStay.Properties.Resources.Logo_128px;
+            this.PBoxMenuLogo.Location = new System.Drawing.Point(0, 9);
+            this.PBoxMenuLogo.Margin = new System.Windows.Forms.Padding(0);
+            this.PBoxMenuLogo.Name = "PBoxMenuLogo";
+            this.PBoxMenuLogo.Size = new System.Drawing.Size(30, 30);
+            this.PBoxMenuLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PBoxMenuLogo.TabIndex = 0;
+            this.PBoxMenuLogo.TabStop = false;
+            // 
+            // LbMenuLogo
+            // 
+            this.LbMenuLogo.AutoSize = true;
+            this.LbMenuLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LbMenuLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.LbMenuLogo.Location = new System.Drawing.Point(33, 12);
+            this.LbMenuLogo.Margin = new System.Windows.Forms.Padding(3);
+            this.LbMenuLogo.Name = "LbMenuLogo";
+            this.LbMenuLogo.Size = new System.Drawing.Size(88, 24);
+            this.LbMenuLogo.TabIndex = 1;
+            this.LbMenuLogo.Text = "EaseStay";
+            this.LbMenuLogo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader1);
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader2);
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader3);
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader4);
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader5);
+            this.flowLayoutPanel1.Controls.Add(this.LbOptionsHeader6);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 64);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(184, 428);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // LbOptionsHeader1
+            // 
+            this.LbOptionsHeader1.AutoSize = true;
+            this.LbOptionsHeader1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader1.Location = new System.Drawing.Point(3, 3);
+            this.LbOptionsHeader1.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader1.Name = "LbOptionsHeader1";
+            this.LbOptionsHeader1.Size = new System.Drawing.Size(82, 16);
+            this.LbOptionsHeader1.TabIndex = 1;
+            this.LbOptionsHeader1.Text = "Alojamentos";
+            // 
+            // LbOptionsHeader2
+            // 
+            this.LbOptionsHeader2.AutoSize = true;
+            this.LbOptionsHeader2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader2.Location = new System.Drawing.Point(3, 25);
+            this.LbOptionsHeader2.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader2.Name = "LbOptionsHeader2";
+            this.LbOptionsHeader2.Size = new System.Drawing.Size(62, 16);
+            this.LbOptionsHeader2.TabIndex = 2;
+            this.LbOptionsHeader2.Text = "Anuncios";
+            // 
+            // LbOptionsHeader3
+            // 
+            this.LbOptionsHeader3.AutoSize = true;
+            this.LbOptionsHeader3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader3.Location = new System.Drawing.Point(3, 47);
+            this.LbOptionsHeader3.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader3.Name = "LbOptionsHeader3";
+            this.LbOptionsHeader3.Size = new System.Drawing.Size(78, 16);
+            this.LbOptionsHeader3.TabIndex = 3;
+            this.LbOptionsHeader3.Text = "Mensagens";
+            // 
+            // LbOptionsHeader4
+            // 
+            this.LbOptionsHeader4.AutoSize = true;
+            this.LbOptionsHeader4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader4.Location = new System.Drawing.Point(3, 69);
+            this.LbOptionsHeader4.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader4.Name = "LbOptionsHeader4";
+            this.LbOptionsHeader4.Size = new System.Drawing.Size(42, 16);
+            this.LbOptionsHeader4.TabIndex = 4;
+            this.LbOptionsHeader4.Text = "Conta";
+            // 
+            // LbOptionsHeader5
+            // 
+            this.LbOptionsHeader5.AutoSize = true;
+            this.LbOptionsHeader5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader5.Location = new System.Drawing.Point(3, 91);
+            this.LbOptionsHeader5.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader5.Name = "LbOptionsHeader5";
+            this.LbOptionsHeader5.Size = new System.Drawing.Size(54, 16);
+            this.LbOptionsHeader5.TabIndex = 5;
+            this.LbOptionsHeader5.Text = "Suporte";
+            // 
+            // LbOptionsHeader6
+            // 
+            this.LbOptionsHeader6.AutoSize = true;
+            this.LbOptionsHeader6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.LbOptionsHeader6.Location = new System.Drawing.Point(3, 113);
+            this.LbOptionsHeader6.Margin = new System.Windows.Forms.Padding(3);
+            this.LbOptionsHeader6.Name = "LbOptionsHeader6";
+            this.LbOptionsHeader6.Size = new System.Drawing.Size(82, 16);
+            this.LbOptionsHeader6.TabIndex = 6;
+            this.LbOptionsHeader6.Text = "Alojamentos";
+            // 
+            // SplLogoOptions
+            // 
+            this.SplLogoOptions.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.SplLogoOptions.Location = new System.Drawing.Point(23, 49);
+            this.SplLogoOptions.Margin = new System.Windows.Forms.Padding(0);
+            this.SplLogoOptions.Name = "SplLogoOptions";
+            this.SplLogoOptions.Size = new System.Drawing.Size(184, 15);
+            this.SplLogoOptions.TabIndex = 3;
+            this.SplLogoOptions.TabStop = false;
+            // 
+            // LbBtnLogout
+            // 
+            this.LbBtnLogout.AutoSize = true;
+            this.LbBtnLogout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LbBtnLogout.Location = new System.Drawing.Point(26, 495);
+            this.LbBtnLogout.Margin = new System.Windows.Forms.Padding(3);
+            this.LbBtnLogout.Name = "LbBtnLogout";
+            this.LbBtnLogout.Size = new System.Drawing.Size(25, 13);
+            this.LbBtnLogout.TabIndex = 4;
+            this.LbBtnLogout.Text = "Sair";
+            this.LbBtnLogout.Click += new System.EventHandler(this.LbBtnLogout_Click);
+            // 
+            // PagesControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TblDashboardLayout);
+            this.Name = "PagesControl";
+            this.Size = new System.Drawing.Size(768, 512);
+            this.TblDashboardLayout.ResumeLayout(false);
+            this.TblMenuLayout.ResumeLayout(false);
+            this.TblMenuLayout.PerformLayout();
+            this.TblMenuLogoLayout.ResumeLayout(false);
+            this.TblMenuLogoLayout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBoxMenuLogo)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel TblDashboardLayout;
+        private System.Windows.Forms.TableLayoutPanel TblMenuLayout;
+        private System.Windows.Forms.PictureBox PBoxMenuLogo;
+        private System.Windows.Forms.Label LbMenuLogo;
+        private System.Windows.Forms.TableLayoutPanel TblMenuLogoLayout;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label LbOptionsHeader3;
+        private System.Windows.Forms.Label LbOptionsHeader4;
+        private System.Windows.Forms.Label LbOptionsHeader5;
+        private System.Windows.Forms.Label LbOptionsHeader1;
+        private System.Windows.Forms.Label LbOptionsHeader2;
+        private System.Windows.Forms.Splitter SplLogoOptions;
+        private System.Windows.Forms.Label LbOptionsHeader6;
+        private Core.Elements.LabelButtonStylized LbBtnLogout;
+    }
+}
---
+++ src/EaseStay/Features/Pages/Presentation/PagesControl.cs
@@ -0,0 +1,44 @@
+﻿using EaseStay.Core;
+using EaseStay.Features.Auth.Domain.Entities;
+using EaseStay.Features.Auth.Presentation;
+using System;
+using System.Windows.Forms;
+
+namespace EaseStay.Features.Pages.Presentation
+{
+    public partial class PagesControl : UserControl
+    {
+        public PagesControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            if (TblDashboardLayout.Width * .3 > 340)
+            { 
+                TblDashboardLayout.ColumnStyles[0].Width = 340;
+                TblDashboardLayout.ColumnStyles[0].SizeType = SizeType.Absolute;
+            }
+            else
+            {
+                TblDashboardLayout.ColumnStyles[0].Width = 30;
+                TblDashboardLayout.ColumnStyles[0].SizeType = SizeType.Percent;
+            }
+        }
+
+        private void LbBtnLogout_Click(object sender, EventArgs e)
+        {
+            Settings.UserUUID = Guid.Empty;
+            Settings.UserFirstName = string.Empty;
+            Settings.UserLastName = string.Empty;
+            Settings.UserEmail = string.Empty;
+
+            Settings.DeletePersistentSession();
+
+            MainForm.Instance.SetControl(new LoginControl());
+        }
+    }
+}
